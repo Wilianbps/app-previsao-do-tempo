40ee3196a1feaf29edbe148b3dfb2425ade4605c
wilian Brito <wilianbpk@gmail.com>
2023-10-18 17:43:01 -0300

    feat: add style in the card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,26 +1,52 @@
-import { Container } from './styles'
+import { Container } from "./styles";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCloud } from "@fortawesome/free-solid-svg-icons/faCloud";
+import { faDroplet } from "@fortawesome/free-solid-svg-icons/faDroplet";
+import { faWind } from "@fortawesome/free-solid-svg-icons/faWind";
 
 export function Card() {
+  const date = new Date();
+  date.setDate(date.getDate() - 7);
 
-  const date = new Date()
-  date.setDate(date.getDate() - 7)
-
-  const formattedDate = date
+  /*  const formattedDate = date
     .toISOString()
-    .split('T')[0]
-    .split('-')
+    .split("T")[0]
+    .split("-")
     .reverse()
-    .join('/')
+    .join("/"); */
 
   return (
     <Container>
-      <section>
-        <header>
-          <h4>Quantidade</h4>
-        </header>
-        <strong>5 Repositórios </strong>
-        <p>desde {formattedDate}</p>
-      </section>
+      <header>
+        <section>
+          <FontAwesomeIcon icon={faCloud} fontSize={40} />
+          <p>nublado</p>
+        </section>
+        <section>
+          <h4>Nova York, US</h4>
+          <p>18/10/2023</p>
+        </section>
+      </header>
+
+      <main>
+        <p>25</p>
+        <span> &deg;C</span>
+      </main>
+
+      <footer>
+        <section className="content_footer">
+          <div className="droplet">
+            <FontAwesomeIcon icon={faDroplet} />
+            <span>48%</span>
+          </div>
+          <div className="wind">
+            <FontAwesomeIcon icon={faWind} />
+            <span>3km/h</span>
+          </div>
+        </section>
+        <button>Adicionar aos favoritos</button>
+      </footer>
     </Container>
-  )
+  );
 }
--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -2,40 +2,89 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   margin: 0 auto;
-  width: 25rem;
-
+  width: 22rem;
   transform: translateY(calc(-50%));
 
-  section {
-    background: ${(props) => props.theme.orange};
-    padding: 1.5rem 2rem;
-    border-radius: 0.25rem;
-    color: ${props => props.theme["base-title"]};
+  background: ${(props) => props.theme["purple-light"]};
+  padding: 1.5rem 2rem;
+  border-radius: 10px;
+  color: ${(props) => props.theme["base-title"]};
 
-    header {
-      font-size: 1.2rem;
-      color: ${props => props.theme.white};
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-    }
+  header {
+    color: ${(props) => props.theme.white};
+    display: flex;
+    align-items: center;
+    gap: 2rem;
 
-    strong {
-      color: var(--shape);
-      display: block;
-      margin-top: 1rem;
-      font-size: 2rem;
-      font-weight: 500;
-      line-height: 3rem;
+    > section {
+      h4 {
+        font-size: 1.2rem;
+      }
+      p {
+        margin-top: 0.2rem;
+        font-size: 0.875rem;
+        font-weight: 500;
+      }
     }
+  }
+
+  main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 0.5rem;
+    margin: 2rem 0;
 
     p {
-      color: var(--shape);
-      display: block;
-      margin-top: 1rem;
-      font-size: 1rem;
-      font-weight: 500;
-      line-height: 2rem;
+      font-size: 3.5rem;
     }
+
+    span {
+      font-size: 1rem;
+    }
+  }
+
+  footer {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem; 
+
+    .content_footer {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      .droplet {
+        font-weight: bold;
+        display: flex;
+        align-items: center;
+        gap: 0.3rem;
+      }
+
+      .wind {
+        font-weight: bold;
+        display: flex;
+        align-items: center;
+        gap: 0.3rem;
+      }
+    }
+
+    button {
+    margin: 0 auto;
+    font-size: 0.875rem;
+    color: #fff;
+    background: ${(props) => props.theme["purple"]};
+    border: 0;
+    max-width: 15rem;
+    padding: 0 0.875rem;
+    border-radius: 0.25rem;
+    height: 3rem;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
   }
 `;
